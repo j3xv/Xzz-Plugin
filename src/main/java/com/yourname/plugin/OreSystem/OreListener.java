99a092817d5ffe38124a961e9a69d614d143bf1a
j-3 <sayzo.dv@gmail.com>
2026-06-30 21:58:34 +0100

    1.2

--- src/main/java/com/yourname/plugin/OreSystem/OreListener.java
+++ src/main/java/com/yourname/plugin/OreSystem/OreListener.java
@@ -3,11 +3,19 @@ package com.yourname.plugin.OreSystem;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import net.kyori.adventure.text.Component;
 
 public class OreListener implements Listener {
+    public int DiamondChance = 10;
+
+    @EventHandler
     public void onClick(InventoryClickEvent event){
 
         if(!(event.getView().getTitle().equals("OreMain"))){
@@ -25,6 +33,14 @@ public class OreListener implements Listener {
         if(type == Material.DIAMOND_BLOCK){
             Inventory Diamondgui = Bukkit.createInventory(null, 27, "Diamond Setting");
             Player player = (Player) event.getWhoClicked();
+            ItemStack diachen = new ItemStack(Material.CLOCK);
+
+            ItemMeta meta = diachen.getItemMeta();
+            meta.displayName(Component.text("Diamond Chance: " + DiamondChance));
+            diachen.setItemMeta(meta);
+
+            Diamondgui.setItem(12, diachen);
+
             player.openInventory(Diamondgui);
 
         }
